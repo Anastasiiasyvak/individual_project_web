e7111995d08936569e47c34e20d5346921fb27de
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-22 18:03:48 +0300

    add video-container

---
+++ script.js
@@ -0,0 +1,28 @@
+// function toggleText() {
+//     var moreText = document.getElementById("moreText");
+//     var toggleButton = document.getElementById("toggleButton");
+//     var hideButton = document.getElementById("hideButton");
+
+//     if (moreText.style.display === "none") {
+//         moreText.style.display = "block";
+//         toggleButton.style.display = "none";
+//         hideButton.style.display = "block";
+//     } else {
+//         moreText.style.display = "none";
+//         toggleButton.style.display = "block";
+//         hideButton.style.display = "none";
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("video");
+
+    video.addEventListener("click", function() {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+});
+
