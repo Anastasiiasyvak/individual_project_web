565d39dba3481b16f87ef2dda68454436320275b
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-22 19:42:26 +0300

    description

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// function toggleText() {
-//     var moreText = document.getElementById("moreText");
-//     var toggleButton = document.getElementById("toggleButton");
-//     var hideButton = document.getElementById("hideButton");
+function toggleText() {
+    var moreText = document.getElementById("moreText");
+    var toggleButton = document.getElementById("toggleButton");
+    var hideButton = document.getElementById("hideButton");
 
-//     if (moreText.style.display === "none") {
-//         moreText.style.display = "block";
-//         toggleButton.style.display = "none";
-//         hideButton.style.display = "block";
-//     } else {
-//         moreText.style.display = "none";
-//         toggleButton.style.display = "block";
-//         hideButton.style.display = "none";
-//     }
-// }
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        toggleButton.style.display = "none";
+        hideButton.style.display = "block";
+    } else {
+        moreText.style.display = "none";
+        toggleButton.style.display = "block";
+        hideButton.style.display = "none";
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById("video");
