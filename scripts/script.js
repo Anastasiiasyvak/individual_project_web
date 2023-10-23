e38212c009e88ff9a2e3152774ded0b741fb014c
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-23 18:06:58 +0300

    inst facebook chanel buttons

--- scripts/script.js
+++ scripts/script.js
@@ -48,3 +48,29 @@ window.onscroll = function() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+
+
+const instagramIcon = document.getElementById("instagram-icon");
+const instagramLink = "https://www.instagram.com/p/CyVc-sVIG6MuxB5M6xLv2QzJjNAefb_MUF9IfA0/?igshid=MzRlODBiNWFlZA==";
+
+instagramIcon.addEventListener("click", function () {
+  window.open(instagramLink, "_blank");
+});
+
+
+const facebookLink = "https://uk-ua.facebook.com/";
+
+const facebookIcon = document.querySelector(".gg-facebook"); 
+
+facebookIcon.addEventListener("click", function () {
+  window.open(facebookLink, "_blank");
+});
+
+
+const chanelIcon = document.getElementById("chanel-icon");
+
+chanelIcon.addEventListener("click", function () {
+  const chanelLink = "https://www.chanel.com/#";
+  window.open(chanelLink, "_blank");
+});
